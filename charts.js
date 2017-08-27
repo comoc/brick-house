c55b6581a510d2febecf434e072291a320b85873
comoc <akihiro.komori@gmail.com>
2017-08-27 19:22:53 +0900

    fix layouts

--- charts.js
+++ charts.js
@@ -1,109 +1,110 @@
-var elemP = document.getElementById('status1');
-var w = elemP.clientWidth;
-var h = elemP.clientHeight;
-h = h - 4;
+function initCharts() {
+	var elemP = document.getElementById('status1');
+	var w = elemP.clientWidth;
+	var h = elemP.clientHeight;
+	h = h - 4;
 
-var labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-var data = [12, 19, 3, 5, 2, 3];
-var bgColors = [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ];
-var borderColors = [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ];
+	var labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+	var data = [12, 19, 3, 5, 2, 3];
+	var bgColors = [
+		'rgba(255, 99, 132, 0.2)',
+		'rgba(54, 162, 235, 0.2)',
+		'rgba(255, 206, 86, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(153, 102, 255, 0.2)',
+		'rgba(255, 159, 64, 0.2)'
+	];
+	var borderColors = [
+		'rgba(255,99,132,1)',
+		'rgba(54, 162, 235, 1)',
+		'rgba(255, 206, 86, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(153, 102, 255, 1)',
+		'rgba(255, 159, 64, 1)'
+	];
 
-var elem = document.getElementById('myChart1');
-elem.setAttribute("width", w);
-elem.setAttribute("height", h);
+	var elem = document.getElementById('myChart1');
+	elem.setAttribute("width", w);
+	elem.setAttribute("height", h);
 
-var ctx = elem.getContext('2d');
-var myChart1 = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: '# of Votes',
-            data: data,
-            backgroundColor: bgColors,
-            borderColor: borderColors,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+	var ctx = elem.getContext('2d');
+	var myChart1 = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: labels,
+			datasets: [{
+				label: '# of Votes',
+				data: data,
+				backgroundColor: bgColors,
+				borderColor: borderColors,
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 
-elem = document.getElementById('myChart2');
-elem.setAttribute("width", w);
-elem.setAttribute("height", h);
+	elem = document.getElementById('myChart2');
+	elem.setAttribute("width", w);
+	elem.setAttribute("height", h);
 
-ctx = elem.getContext('2d');
-var myChart2 = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: '# of Votes',
-            data: data,
-            backgroundColor: bgColors,
-            borderColor: borderColors,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+	ctx = elem.getContext('2d');
+	var myChart2 = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: labels,
+			datasets: [{
+				label: '# of Votes',
+				data: data,
+				backgroundColor: bgColors,
+				borderColor: borderColors,
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 
-elem = document.getElementById('myChart3');
-elem.setAttribute("width", w);
-elem.setAttribute("height", h);
+	elem = document.getElementById('myChart3');
+	elem.setAttribute("width", w);
+	elem.setAttribute("height", h);
 
-ctx = elem.getContext('2d');
-var myChart3 = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: '# of Votes',
-            data: data,
-            backgroundColor: bgColors,
-            borderColor: borderColors,
-            borderWidth: 1
-        }],
+	ctx = elem.getContext('2d');
+	var myChart3 = new Chart(ctx, {
+		type: 'polarArea',
+		data: {
+			labels: labels,
+			datasets: [{
+				label: '# of Votes',
+				data: data,
+				backgroundColor: bgColors,
+				borderColor: borderColors,
+				borderWidth: 1
+			}],
 
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+}
